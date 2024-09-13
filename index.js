f47328fbeaa51149c26d915030e64829406620f1
recoskyler <38231748+recoskyler@users.noreply.github.com>
2024-09-13 21:52:56 +0300

    refactor: increase rate limit

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const app = express();
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 Mins
-  max: 100,
+  windowMs: 1000, // 1 Second
+  max: 3,
 });
 
 app.use(limiter);
